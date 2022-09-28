7308dd0667a57cc4e2aab40a69883eaee63b0193
Henrique de Freitas Dias <60013916+henriquefdias@users.noreply.github.com>
2022-09-27 21:11:41 -0300

    Initial commit

---
+++ src/main/java/com/henriquefdias/sistemapedidosbackend/SistemaPedidosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.henriquefdias.sistemapedidosbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaPedidosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaPedidosBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/henriquefdias/sistemapedidosbackend/SistemaPedidosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henriquefdias.sistemapedidosbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaPedidosBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

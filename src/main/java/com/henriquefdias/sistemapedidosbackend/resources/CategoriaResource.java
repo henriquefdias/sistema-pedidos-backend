99d3347216493cc2dba52b7e240429d1917b8352
Henrique de Freitas Dias <60013916+henriquefdias@users.noreply.github.com>
2022-09-30 08:33:53 -0300

    Update CategoriaResource.java

--- src/main/java/com/henriquefdias/sistemapedidosbackend/resources/CategoriaResource.java
+++ src/main/java/com/henriquefdias/sistemapedidosbackend/resources/CategoriaResource.java
@@ -1,5 +1,10 @@
 package com.henriquefdias.sistemapedidosbackend.resources;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/categorias")
 public class CategoriaResource {
 
 }

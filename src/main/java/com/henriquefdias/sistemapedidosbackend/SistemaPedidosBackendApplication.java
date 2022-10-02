801f0dd05f52242fe35fc74f17862baafe8639b0
Henrique de Freitas Dias <60013916+henriquefdias@users.noreply.github.com>
2022-10-02 10:28:28 -0300

    Update SistemaPedidosBackendApplication.java

--- src/main/java/com/henriquefdias/sistemapedidosbackend/SistemaPedidosBackendApplication.java
+++ src/main/java/com/henriquefdias/sistemapedidosbackend/SistemaPedidosBackendApplication.java
@@ -8,7 +8,6 @@ public class SistemaPedidosBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SistemaPedidosBackendApplication.class, args);
-		System.out.println("Hello World!");
 	}
 
 }

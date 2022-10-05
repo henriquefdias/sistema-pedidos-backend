38e4b6c4ab65ad9aed362fffec9420586701ad8f
Henrique de Freitas Dias <60013916+henriquefdias@users.noreply.github.com>
2022-10-05 08:02:13 -0300

    Update Categoria.java

--- src/main/java/com/henriquefdias/sistemapedidosbackend/domain/Categoria.java
+++ src/main/java/com/henriquefdias/sistemapedidosbackend/domain/Categoria.java
@@ -4,5 +4,14 @@ public class Categoria {
 	
 	private Integer id;
 	private String nome;
+	
+	public Categoria() {
+	}
+
+	public Categoria(Integer id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
 
 }

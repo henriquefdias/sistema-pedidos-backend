e067c9fe8ae6d58623b7b606f181350004d29b05
Henrique de Freitas Dias <60013916+henriquefdias@users.noreply.github.com>
2022-10-07 08:08:10 -0300

    Update Categoria.java

--- src/main/java/com/henriquefdias/sistemapedidosbackend/domain/Categoria.java
+++ src/main/java/com/henriquefdias/sistemapedidosbackend/domain/Categoria.java
@@ -1,5 +1,7 @@
 package com.henriquefdias.sistemapedidosbackend.domain;
 
+import java.util.Objects;
+
 public class Categoria {
 	
 	private Integer id;
@@ -28,6 +30,23 @@ public class Categoria {
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		return Objects.equals(id, other.id);
 	}
 
 }

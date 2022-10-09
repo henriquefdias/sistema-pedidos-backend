62aad05dbfe037a137573af3bb79b0177f26d99c
Henrique de Freitas Dias <60013916+henriquefdias@users.noreply.github.com>
2022-10-09 10:12:05 -0300

    Update Categoria.java

--- src/main/java/com/henriquefdias/sistemapedidosbackend/domain/Categoria.java
+++ src/main/java/com/henriquefdias/sistemapedidosbackend/domain/Categoria.java
@@ -4,11 +4,8 @@ import java.io.Serializable;
 import java.util.Objects;
 
 public class Categoria implements Serializable {
-	
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
+	
 	private Integer id;
 	private String nome;
 	

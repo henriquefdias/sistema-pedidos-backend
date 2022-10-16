2c2098e2294a5ee10ded155d61264b2dde63740b
Henrique de Freitas Dias <60013916+henriquefdias@users.noreply.github.com>
2022-10-16 12:56:04 -0300

    Update CategoriaRepository.java

--- src/main/java/com/henriquefdias/sistemapedidosbackend/repositories/CategoriaRepository.java
+++ src/main/java/com/henriquefdias/sistemapedidosbackend/repositories/CategoriaRepository.java
@@ -1,5 +1,13 @@
 package com.henriquefdias.sistemapedidosbackend.repositories;
 
-public class CategoriaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.henriquefdias.sistemapedidosbackend.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+	
+	
 
 }

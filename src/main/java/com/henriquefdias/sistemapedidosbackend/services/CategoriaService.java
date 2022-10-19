2e7fe599ee95a7ad4e6ffcfc462f0c2c600b44c6
Henrique de Freitas Dias <60013916+henriquefdias@users.noreply.github.com>
2022-10-19 08:24:25 -0300

    Update CategoriaService.java

--- src/main/java/com/henriquefdias/sistemapedidosbackend/services/CategoriaService.java
+++ src/main/java/com/henriquefdias/sistemapedidosbackend/services/CategoriaService.java
@@ -2,7 +2,13 @@ package com.henriquefdias.sistemapedidosbackend.services;
 
 import org.springframework.stereotype.Service;
 
+import com.henriquefdias.sistemapedidosbackend.domain.Categoria;
+
 @Service
 public class CategoriaService {
 
+	public Categoria buscar() {
+		
+	}
+	
 }

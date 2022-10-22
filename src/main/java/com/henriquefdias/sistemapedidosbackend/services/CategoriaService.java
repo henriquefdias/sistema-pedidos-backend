3932589b6711b84b35996354c78d09e9daa9af7d
Henrique de Freitas Dias <60013916+henriquefdias@users.noreply.github.com>
2022-10-22 17:03:22 -0300

    Update CategoriaService.java

--- src/main/java/com/henriquefdias/sistemapedidosbackend/services/CategoriaService.java
+++ src/main/java/com/henriquefdias/sistemapedidosbackend/services/CategoriaService.java
@@ -1,5 +1,7 @@
 package com.henriquefdias.sistemapedidosbackend.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,7 +15,8 @@ public class CategoriaService {
 	private CategoriaRepository repo;
 	
 	public Categoria buscar(Integer id) {
-		Categoria obj = repo.findOne(id);
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
 	}
 	
 }

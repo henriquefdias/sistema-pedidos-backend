f8a6f37573ce30178941568098283d4836090e9d
Henrique de Freitas Dias <60013916+henriquefdias@users.noreply.github.com>
2022-10-24 08:44:50 -0300

    Update CategoriaResource.java

--- src/main/java/com/henriquefdias/sistemapedidosbackend/resources/CategoriaResource.java
+++ src/main/java/com/henriquefdias/sistemapedidosbackend/resources/CategoriaResource.java
@@ -3,6 +3,7 @@ package com.henriquefdias.sistemapedidosbackend.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,7 +16,7 @@ import com.henriquefdias.sistemapedidosbackend.domain.Categoria;
 public class CategoriaResource {
 	
 	@RequestMapping(value="/{id}", method = RequestMethod.GET)
-	public List<Categoria> find(@PathVariable Integer id) {
+	public ResponseEntity<?> find(@PathVariable Integer id) {
 		
 		Categoria cat1 = new Categoria(1, "Informática");
 		Categoria cat2 = new Categoria(2, "Escritório");

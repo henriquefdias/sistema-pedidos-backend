02350033aa311149668af55622ea04cb7db1b797
Henrique de Freitas Dias <60013916+henriquefdias@users.noreply.github.com>
2022-10-25 08:47:44 -0300

    Update CategoriaResource.java

--- src/main/java/com/henriquefdias/sistemapedidosbackend/resources/CategoriaResource.java
+++ src/main/java/com/henriquefdias/sistemapedidosbackend/resources/CategoriaResource.java
@@ -3,6 +3,7 @@ package com.henriquefdias.sistemapedidosbackend.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,10 +11,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.henriquefdias.sistemapedidosbackend.domain.Categoria;
+import com.henriquefdias.sistemapedidosbackend.services.CategoriaService;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
+
+	@Autowired
+	private CategoriaService service;
 	
 	@RequestMapping(value="/{id}", method = RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {

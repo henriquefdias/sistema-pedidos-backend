6202bac3de55c934bb16cb174d1eee80646c2e1f
Henrique de Freitas Dias <60013916+henriquefdias@users.noreply.github.com>
2022-10-31 07:59:05 -0300

    Update SistemaPedidosBackendApplication.java

--- src/main/java/com/henriquefdias/sistemapedidosbackend/SistemaPedidosBackendApplication.java
+++ src/main/java/com/henriquefdias/sistemapedidosbackend/SistemaPedidosBackendApplication.java
@@ -1,13 +1,20 @@
 package com.henriquefdias.sistemapedidosbackend;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.henriquefdias.sistemapedidosbackend.domain.Categoria;
+import com.henriquefdias.sistemapedidosbackend.repositories.CategoriaRepository;
 
 @SpringBootApplication
 public class SistemaPedidosBackendApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SistemaPedidosBackendApplication.class, args);
@@ -19,6 +26,7 @@ public class SistemaPedidosBackendApplication implements CommandLineRunner {
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }

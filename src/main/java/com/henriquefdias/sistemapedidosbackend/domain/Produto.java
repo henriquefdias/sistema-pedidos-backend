c285817a21f33c8a705ebf6f51b4cabf81bd1c2f
Henrique de Freitas Dias <60013916+henriquefdias@users.noreply.github.com>
2022-11-05 07:06:35 -0300

    Update Produto.java

--- src/main/java/com/henriquefdias/sistemapedidosbackend/domain/Produto.java
+++ src/main/java/com/henriquefdias/sistemapedidosbackend/domain/Produto.java
@@ -1,9 +1,14 @@
 package com.henriquefdias.sistemapedidosbackend.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Produto {
 	
 	private Integer id;
 	private String nome;
 	private Double preco;
+	
+	private List<Categoria> categorias = new ArrayList<>();
 
 }

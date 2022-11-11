3888b3c4c3c0905e284aab9c6eb53963ad6b1f49
Henrique de Freitas Dias <60013916+henriquefdias@users.noreply.github.com>
2022-11-11 08:30:18 -0300

    Update Produto.java

--- src/main/java/com/henriquefdias/sistemapedidosbackend/domain/Produto.java
+++ src/main/java/com/henriquefdias/sistemapedidosbackend/domain/Produto.java
@@ -1,10 +1,12 @@
 package com.henriquefdias.sistemapedidosbackend.domain;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-public class Produto {
+public class Produto implements Serializable {
+	private static final long serialVersionUID = 1L;
 	
 	private Integer id;
 	private String nome;

5bef4909aa8a2ae26775cd2371faa5b90a8cdb89
Henrique de Freitas Dias <60013916+henriquefdias@users.noreply.github.com>
2022-11-13 17:09:25 -0300

    Update Produto.java

--- src/main/java/com/henriquefdias/sistemapedidosbackend/domain/Produto.java
+++ src/main/java/com/henriquefdias/sistemapedidosbackend/domain/Produto.java
@@ -6,11 +6,13 @@ import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 public class Produto implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
 	private Integer id;
 	private String nome;
 	private Double preco;

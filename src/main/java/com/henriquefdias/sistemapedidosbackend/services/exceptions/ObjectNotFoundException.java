ce1ec3267b070eadacd75c701f2305e1cb8c0f72
Henrique de Freitas Dias <60013916+henriquefdias@users.noreply.github.com>
2022-12-04 09:37:03 -0300

    Update ObjectNotFoundException.java

--- src/main/java/com/henriquefdias/sistemapedidosbackend/services/exceptions/ObjectNotFoundException.java
+++ src/main/java/com/henriquefdias/sistemapedidosbackend/services/exceptions/ObjectNotFoundException.java
@@ -1,7 +1,11 @@
 package com.henriquefdias.sistemapedidosbackend.services.exceptions;
 
 public class ObjectNotFoundException extends RuntimeException {
-	
+
 	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
 
 }

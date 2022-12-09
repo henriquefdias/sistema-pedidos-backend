d090ae4e25099a78408c96a525e48270ef6b928c
Henrique de Freitas Dias <60013916+henriquefdias@users.noreply.github.com>
2022-12-09 08:29:42 -0300

    Update ResourceExceptionHandler.java

--- src/main/java/com/henriquefdias/sistemapedidosbackend/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/henriquefdias/sistemapedidosbackend/resources/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,8 @@
 package com.henriquefdias.sistemapedidosbackend.resources.exceptions;
 
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
 public class ResourceExceptionHandler {
 
 }

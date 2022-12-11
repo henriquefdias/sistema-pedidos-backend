efa1773e032e48d80f872ad2678c3530a0a3e7f8
Henrique de Freitas Dias <60013916+henriquefdias@users.noreply.github.com>
2022-12-11 12:37:22 -0300

    Update StandardError.java

--- src/main/java/com/henriquefdias/sistemapedidosbackend/resources/exceptions/StandardError.java
+++ src/main/java/com/henriquefdias/sistemapedidosbackend/resources/exceptions/StandardError.java
@@ -1,5 +1,9 @@
 package com.henriquefdias.sistemapedidosbackend.resources.exceptions;
 
 public class StandardError {
+	
+	private Integer status;
+	private String msg;
+	private Long timeStamp;
 
 }

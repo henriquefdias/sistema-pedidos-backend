f765a2edfa45df6b0fc9f5dd61b5b2fc122dc530
Henrique de Freitas Dias <60013916+henriquefdias@users.noreply.github.com>
2022-12-16 09:18:00 -0300

    Update ResourceExceptionHandler.java

--- src/main/java/com/henriquefdias/sistemapedidosbackend/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/henriquefdias/sistemapedidosbackend/resources/exceptions/ResourceExceptionHandler.java
@@ -1,8 +1,17 @@
 package com.henriquefdias.sistemapedidosbackend.resources.exceptions;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+
+import com.henriquefdias.sistemapedidosbackend.services.exceptions.ObjectNotFoundException;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
 
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
+		
+	}
+	
 }

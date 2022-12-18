249b6fa5201c153539fbb2de44e33f7ac5827415
Henrique de Freitas Dias <60013916+henriquefdias@users.noreply.github.com>
2022-12-18 11:11:08 -0300

    Update ResourceExceptionHandler.java

--- src/main/java/com/henriquefdias/sistemapedidosbackend/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/henriquefdias/sistemapedidosbackend/resources/exceptions/ResourceExceptionHandler.java
@@ -14,7 +14,7 @@ public class ResourceExceptionHandler {
 	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
 		
 		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
-		
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
 	}
 	
 }

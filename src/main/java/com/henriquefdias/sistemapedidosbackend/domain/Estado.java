732739682f04be176a36cad4ee11ec24d0573f0b
Henrique de Freitas Dias <60013916+henriquefdias@users.noreply.github.com>
2022-12-28 17:23:23 -0300

    Update Estado.java

--- src/main/java/com/henriquefdias/sistemapedidosbackend/domain/Estado.java
+++ src/main/java/com/henriquefdias/sistemapedidosbackend/domain/Estado.java
@@ -1,8 +1,13 @@
 package com.henriquefdias.sistemapedidosbackend.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Estado {
 	
 	private Integer id;
 	private String nome;
+	
+	private List<Cidade> cidades = new ArrayList<>();
 
 }

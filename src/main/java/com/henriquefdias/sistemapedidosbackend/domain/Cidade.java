f3adadd6fe3069bc5f178fc01a3bbe76594be834
Henrique de Freitas Dias <60013916+henriquefdias@users.noreply.github.com>
2023-01-03 08:33:49 -0300

    Update Cidade.java

--- src/main/java/com/henriquefdias/sistemapedidosbackend/domain/Cidade.java
+++ src/main/java/com/henriquefdias/sistemapedidosbackend/domain/Cidade.java
@@ -17,5 +17,29 @@ public class Cidade {
 		this.nome = nome;
 		this.estado = estado;
 	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Estado getEstado() {
+		return estado;
+	}
+
+	public void setEstado(Estado estado) {
+		this.estado = estado;
+	}
 	
 }

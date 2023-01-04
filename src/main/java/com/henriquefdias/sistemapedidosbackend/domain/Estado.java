ef4fdf4d498224bbf7ca4e4a90da30040f2c6535
Henrique de Freitas Dias <60013916+henriquefdias@users.noreply.github.com>
2023-01-04 08:29:45 -0300

    Update Estado.java

--- src/main/java/com/henriquefdias/sistemapedidosbackend/domain/Estado.java
+++ src/main/java/com/henriquefdias/sistemapedidosbackend/domain/Estado.java
@@ -19,4 +19,28 @@ public class Estado {
 		this.id = id;
 		this.nome = nome;
 	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public List<Cidade> getCidades() {
+		return cidades;
+	}
+
+	public void setCidades(List<Cidade> cidades) {
+		this.cidades = cidades;
+	}
 }

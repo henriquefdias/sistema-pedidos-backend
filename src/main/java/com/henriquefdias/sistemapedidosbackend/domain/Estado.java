f7e06f8f4726a2dff142ecae81c5d7bc5ee6f20c
Henrique de Freitas Dias <60013916+henriquefdias@users.noreply.github.com>
2023-01-15 10:58:41 -0300

    Update Estado.java

--- src/main/java/com/henriquefdias/sistemapedidosbackend/domain/Estado.java
+++ src/main/java/com/henriquefdias/sistemapedidosbackend/domain/Estado.java
@@ -5,11 +5,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+@Entity
 public class Estado implements Serializable {
 	private static final long serialVersionUID = 1L;
 	

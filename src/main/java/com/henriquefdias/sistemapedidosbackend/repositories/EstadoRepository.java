066bc107b53fdc30b6f8387da3c757335d21fe88
Henrique de Freitas Dias <60013916+henriquefdias@users.noreply.github.com>
2023-01-26 08:41:27 -0300

    Update EstadoRepository.java

--- src/main/java/com/henriquefdias/sistemapedidosbackend/repositories/EstadoRepository.java
+++ src/main/java/com/henriquefdias/sistemapedidosbackend/repositories/EstadoRepository.java
@@ -3,10 +3,10 @@ package com.henriquefdias.sistemapedidosbackend.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.henriquefdias.sistemapedidosbackend.domain.Cidade;
+import com.henriquefdias.sistemapedidosbackend.domain.Estado;
 
 @Repository
-public interface EstadoRepository extends JpaRepository<Cidade, Integer>{
+public interface EstadoRepository extends JpaRepository<Estado, Integer>{
 	
 	
 

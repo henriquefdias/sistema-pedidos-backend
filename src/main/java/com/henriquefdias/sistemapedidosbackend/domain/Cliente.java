5df7066a2534f86899b8e6082c8fb80d5b2566e3
Henrique de Freitas Dias <60013916+henriquefdias@users.noreply.github.com>
2023-02-15 08:21:28 -0300

    Update Cliente.java

--- src/main/java/com/henriquefdias/sistemapedidosbackend/domain/Cliente.java
+++ src/main/java/com/henriquefdias/sistemapedidosbackend/domain/Cliente.java
@@ -1,5 +1,13 @@
 package com.henriquefdias.sistemapedidosbackend.domain;
 
+import com.henriquefdias.sistemapedidosbackend.domain.enums.TipoCliente;
+
 public class Cliente {
+	
+	private Integer id;
+	private String nome;
+	private String email;
+	private String cpfOuCnpj;
+	private TipoCliente tipo;
 
 }

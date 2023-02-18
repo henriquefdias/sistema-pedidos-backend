976ef5c35c2feeafa8178e0333fe3113befd551e
Henrique de Freitas Dias <60013916+henriquefdias@users.noreply.github.com>
2023-02-18 18:17:37 -0300

    Update Endereco.java

--- src/main/java/com/henriquefdias/sistemapedidosbackend/domain/Endereco.java
+++ src/main/java/com/henriquefdias/sistemapedidosbackend/domain/Endereco.java
@@ -1,5 +1,13 @@
 package com.henriquefdias.sistemapedidosbackend.domain;
 
 public class Endereco {
+	
+	private Integer id;
+	private String logradouro;
+	private String numero;
+	private String complemento;
+	private String bairro;
+	private String cep;
+	
 
 }

77421ed6c184d5c8fca749388bbe2baf756f821a
Henrique de Freitas Dias <60013916+henriquefdias@users.noreply.github.com>
2023-03-22 08:55:54 -0300

    Update Cliente.java

--- src/main/java/com/henriquefdias/sistemapedidosbackend/domain/Cliente.java
+++ src/main/java/com/henriquefdias/sistemapedidosbackend/domain/Cliente.java
@@ -11,6 +11,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 import com.henriquefdias.sistemapedidosbackend.domain.enums.TipoCliente;
 
@@ -26,6 +27,7 @@ public class Cliente implements Serializable {
 	private String cpfOuCnpj;
 	private Integer tipo;
 	
+	@OneToMany(mappedBy = "cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
 	
 	private Set<String> telefones = new HashSet<>();

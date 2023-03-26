e50fa79bc28c0cc19fccab9cab8524efa9885862
Henrique de Freitas Dias <60013916+henriquefdias@users.noreply.github.com>
2023-03-26 10:02:56 -0300

    Update SistemaPedidosBackendApplication.java

--- src/main/java/com/henriquefdias/sistemapedidosbackend/SistemaPedidosBackendApplication.java
+++ src/main/java/com/henriquefdias/sistemapedidosbackend/SistemaPedidosBackendApplication.java
@@ -69,6 +69,8 @@ public class SistemaPedidosBackendApplication implements CommandLineRunner {
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 		
 		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		
+		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
 	}
 
 }

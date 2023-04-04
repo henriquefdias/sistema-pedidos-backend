ab560058863efe2930dc1fe2384a84d23cfa4b74
Henrique de Freitas Dias <60013916+henriquefdias@users.noreply.github.com>
2023-04-04 11:26:14 -0300

    Update SistemaPedidosBackendApplication.java

--- src/main/java/com/henriquefdias/sistemapedidosbackend/SistemaPedidosBackendApplication.java
+++ src/main/java/com/henriquefdias/sistemapedidosbackend/SistemaPedidosBackendApplication.java
@@ -80,6 +80,8 @@ public class SistemaPedidosBackendApplication implements CommandLineRunner {
 		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
 		
 		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		
+		clienteRepository.saveAll(Arrays.asList(cli1));
 	}
 
 }

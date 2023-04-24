e0c5e7969a227bb2aa61248993c0e5b1fffd2ceb
Henrique de Freitas Dias <60013916+henriquefdias@users.noreply.github.com>
2023-04-24 08:10:57 -0300

    Update EstadoPagamento.java

--- src/main/java/com/henriquefdias/sistemapedidosbackend/domain/enums/EstadoPagamento.java
+++ src/main/java/com/henriquefdias/sistemapedidosbackend/domain/enums/EstadoPagamento.java
@@ -6,4 +6,37 @@ public enum EstadoPagamento {
 	QUITADO(2, "Quitado"),
 	CANCELADO(3, "Cancelado");
 	
+	private int cod;
+	private String descricao;
+	
+	private TipoCliente(int cod, String descricao) {
+		this.cod = cod;
+		this.descricao = descricao;
+	}
+	
+	public int getCod() {
+		return cod;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	public static TipoCliente toEnum(Integer cod) {
+		
+		if(cod == null) {
+			return null;
+		}
+		
+		for(TipoCliente x : TipoCliente.values()) {
+			if(cod.equals(x.getCod())) {
+				return x;
+			}
+		}
+		
+		throw new IllegalArgumentException("Id inválido: " + cod);
+	}
+}
+
+	
 }

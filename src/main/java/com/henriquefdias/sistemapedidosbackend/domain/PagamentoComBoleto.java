5d7f34a160ea338744bb93700f6752fb8ffad88a
Henrique de Freitas Dias <60013916+henriquefdias@users.noreply.github.com>
2023-05-15 08:59:42 -0300

    Update PagamentoComBoleto.java

--- src/main/java/com/henriquefdias/sistemapedidosbackend/domain/PagamentoComBoleto.java
+++ src/main/java/com/henriquefdias/sistemapedidosbackend/domain/PagamentoComBoleto.java
@@ -2,11 +2,17 @@ package com.henriquefdias.sistemapedidosbackend.domain;
 
 import java.util.Date;
 
+import com.henriquefdias.sistemapedidosbackend.domain.enums.EstadoPagamento;
+
 public class PagamentoComBoleto extends Pagamento {
 	
 	private Date dataVencimento;
 	private Date dataPagamento;
 
 	public PagamentoComBoleto() {
+	}
+
+	public PagamentoComBoleto(Integer id, EstadoPagamento estado, Pedido pedido) {
+		super(id, estado, pedido);
 	}
 }

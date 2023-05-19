4273575e47b4aaec49870d555ce27fed5af0c62d
Henrique de Freitas Dias <60013916+henriquefdias@users.noreply.github.com>
2023-05-19 08:42:28 -0300

    Update PagamentoComCartao.java

--- src/main/java/com/henriquefdias/sistemapedidosbackend/domain/PagamentoComCartao.java
+++ src/main/java/com/henriquefdias/sistemapedidosbackend/domain/PagamentoComCartao.java
@@ -3,5 +3,9 @@ package com.henriquefdias.sistemapedidosbackend.domain;
 public class PagamentoComCartao extends Pagamento {
 	
 	private Integer numeroDeParcelas;
+	
+	public PagamentoComCartao() {
+		
+	}
 
 }

16902c8100277d5272fba79de94ef8837619a192
Henrique de Freitas Dias <60013916+henriquefdias@users.noreply.github.com>
2023-05-20 18:26:25 -0300

    Update PagamentoComCartao.java

--- src/main/java/com/henriquefdias/sistemapedidosbackend/domain/PagamentoComCartao.java
+++ src/main/java/com/henriquefdias/sistemapedidosbackend/domain/PagamentoComCartao.java
@@ -1,11 +1,18 @@
 package com.henriquefdias.sistemapedidosbackend.domain;
 
+import com.henriquefdias.sistemapedidosbackend.domain.enums.EstadoPagamento;
+
 public class PagamentoComCartao extends Pagamento {
 	
 	private Integer numeroDeParcelas;
 	
 	public PagamentoComCartao() {
 		
+	}
+
+	public PagamentoComCartao(Integer id, EstadoPagamento estado, Pedido pedido) {
+		super(id, estado, pedido);
+		// TODO Auto-generated constructor stub
 	}
 
 }

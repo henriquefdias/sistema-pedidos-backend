0088142edb8a9cb2786ed0250d630fe25cd07158
Henrique de Freitas Dias <60013916+henriquefdias@users.noreply.github.com>
2023-05-26 08:59:05 -0300

    Update Pagamento.java

--- src/main/java/com/henriquefdias/sistemapedidosbackend/domain/Pagamento.java
+++ src/main/java/com/henriquefdias/sistemapedidosbackend/domain/Pagamento.java
@@ -18,5 +18,29 @@ public class Pagamento {
 		this.estado = estado;
 		this.pedido = pedido;
 	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public EstadoPagamento getEstado() {
+		return estado;
+	}
+
+	public void setEstado(EstadoPagamento estado) {
+		this.estado = estado;
+	}
+
+	public Pedido getPedido() {
+		return pedido;
+	}
+
+	public void setPedido(Pedido pedido) {
+		this.pedido = pedido;
+	}
 	
 }

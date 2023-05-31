a5b5e7426d7217787c867db8d9aa9181776674a0
Henrique de Freitas Dias <60013916+henriquefdias@users.noreply.github.com>
2023-05-31 08:33:12 -0300

    Update Pedido.java

--- src/main/java/com/henriquefdias/sistemapedidosbackend/domain/Pedido.java
+++ src/main/java/com/henriquefdias/sistemapedidosbackend/domain/Pedido.java
@@ -1,6 +1,7 @@
 package com.henriquefdias.sistemapedidosbackend.domain;
 
 import java.util.Date;
+import java.util.Objects;
 
 public class Pedido {
 
@@ -63,6 +64,23 @@ public class Pedido {
 
 	public void setEnderecoDeEntrega(Endereco enderecoDeEntrega) {
 		this.enderecoDeEntrega = enderecoDeEntrega;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pedido other = (Pedido) obj;
+		return Objects.equals(id, other.id);
 	}
 	
 }

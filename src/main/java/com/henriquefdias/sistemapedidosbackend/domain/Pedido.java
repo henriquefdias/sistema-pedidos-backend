6e9921e30bf2c27be4365ebc524fed3b38c04e5b
Henrique de Freitas Dias <60013916+henriquefdias@users.noreply.github.com>
2023-06-02 17:34:16 -0300

    Update Pedido.java

--- src/main/java/com/henriquefdias/sistemapedidosbackend/domain/Pedido.java
+++ src/main/java/com/henriquefdias/sistemapedidosbackend/domain/Pedido.java
@@ -1,9 +1,11 @@
 package com.henriquefdias.sistemapedidosbackend.domain;
 
+import java.io.Serializable;
 import java.util.Date;
 import java.util.Objects;
 
-public class Pedido {
+public class Pedido  implements Serializable {
+	private static final long serialVersionUID = 1L;
 
 	private Integer id;
 	private Date instante;

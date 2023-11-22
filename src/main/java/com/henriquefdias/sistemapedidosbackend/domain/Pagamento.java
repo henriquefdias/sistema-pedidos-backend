f040037dca4deafbe477ee6196cb664d1046af10
Henrique de Freitas Dias <60013916+henriquefdias@users.noreply.github.com>
2023-11-22 08:58:37 -0300

    Update Pagamento.java

--- src/main/java/com/henriquefdias/sistemapedidosbackend/domain/Pagamento.java
+++ src/main/java/com/henriquefdias/sistemapedidosbackend/domain/Pagamento.java
@@ -5,6 +5,7 @@ import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 
 import com.henriquefdias.sistemapedidosbackend.domain.enums.EstadoPagamento;
@@ -18,6 +19,7 @@ public class Pagamento implements Serializable {
 	private EstadoPagamento estado;
 
 	@OneToOne
+	@JoinColumn(name="pedido_id")
 	private Pedido pedido;
 	
 	public Pagamento() {

de4b0cf37ce0cbe31e25220a1da60ce5d2b1def6
Henrique de Freitas Dias <60013916+henriquefdias@users.noreply.github.com>
2023-12-02 10:25:48 -0300

    Update PagamentoComCartao.java

--- src/main/java/com/henriquefdias/sistemapedidosbackend/domain/PagamentoComCartao.java
+++ src/main/java/com/henriquefdias/sistemapedidosbackend/domain/PagamentoComCartao.java
@@ -1,7 +1,10 @@
 package com.henriquefdias.sistemapedidosbackend.domain;
 
+import javax.persistence.Entity;
+
 import com.henriquefdias.sistemapedidosbackend.domain.enums.EstadoPagamento;
 
+@Entity
 public class PagamentoComCartao extends Pagamento {
 	private static final long serialVersionUID = 1L;
 	

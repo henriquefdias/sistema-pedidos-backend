8d63ef2a3e9bf3ce7ce7b39c5c74fcfa2743328c
Henrique de Freitas Dias <60013916+henriquefdias@users.noreply.github.com>
2023-12-24 13:06:40 -0300

    Update SistemaPedidosBackendApplication.java

--- src/main/java/com/henriquefdias/sistemapedidosbackend/SistemaPedidosBackendApplication.java
+++ src/main/java/com/henriquefdias/sistemapedidosbackend/SistemaPedidosBackendApplication.java
@@ -96,7 +96,7 @@ public class SistemaPedidosBackendApplication implements CommandLineRunner {
 		Pedido ped1 = new Pedido(null, sdf.parse("30/07/2017 10:32"), cli1, e1);
 		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
 		
-		Pagamento pagto1 = new PagamentoComCartao();
+		Pagamento pagto1 = new PagamentoComCartao(null);
 	}
 
 }

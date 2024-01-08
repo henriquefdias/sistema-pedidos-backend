31c829e23162b60e05956958833047b165d40c6d
Henrique de Freitas Dias <60013916+henriquefdias@users.noreply.github.com>
2024-01-08 13:39:07 -0300

    Update PedidoRepository.java

--- src/main/java/com/henriquefdias/sistemapedidosbackend/repositories/PedidoRepository.java
+++ src/main/java/com/henriquefdias/sistemapedidosbackend/repositories/PedidoRepository.java
@@ -4,9 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.henriquefdias.sistemapedidosbackend.domain.Categoria;
+import com.henriquefdias.sistemapedidosbackend.domain.Pedido;
 
 @Repository
-public interface PedidoRepository extends JpaRepository<Categoria, Integer>{
+public interface PedidoRepository extends JpaRepository<Pedido, Integer>{
 	
 	
 

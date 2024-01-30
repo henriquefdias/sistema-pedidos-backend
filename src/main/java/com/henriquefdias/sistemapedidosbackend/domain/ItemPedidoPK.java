6b07a729ecfb5c35e5d50c73dac4729ae0c37390
Henrique de Freitas Dias <60013916+henriquefdias@users.noreply.github.com>
2024-01-30 08:30:17 -0300

    Update ItemPedidoPK.java

--- src/main/java/com/henriquefdias/sistemapedidosbackend/domain/ItemPedidoPK.java
+++ src/main/java/com/henriquefdias/sistemapedidosbackend/domain/ItemPedidoPK.java
@@ -3,7 +3,8 @@ package com.henriquefdias.sistemapedidosbackend.domain;
 import java.io.Serializable;
 
 public class ItemPedidoPK implements Serializable{
-
+	private static final long serialVersionUID = 1L;
+	
 	private Pedido pedido;
 	private Produto produto;
 }

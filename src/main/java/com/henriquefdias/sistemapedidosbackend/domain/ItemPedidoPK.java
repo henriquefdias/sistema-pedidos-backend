fa5767668e28547702a72a56bca1dbc931be3cfb
Henrique de Freitas Dias <60013916+henriquefdias@users.noreply.github.com>
2024-02-01 20:16:44 -0300

    Update ItemPedidoPK.java

--- src/main/java/com/henriquefdias/sistemapedidosbackend/domain/ItemPedidoPK.java
+++ src/main/java/com/henriquefdias/sistemapedidosbackend/domain/ItemPedidoPK.java
@@ -1,6 +1,7 @@
 package com.henriquefdias.sistemapedidosbackend.domain;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 public class ItemPedidoPK implements Serializable{
 	private static final long serialVersionUID = 1L;
@@ -19,4 +20,20 @@ public class ItemPedidoPK implements Serializable{
 	public void setProduto(Produto produto) {
 		this.produto = produto;
 	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(pedido, produto);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ItemPedidoPK other = (ItemPedidoPK) obj;
+		return Objects.equals(pedido, other.pedido) && Objects.equals(produto, other.produto);
+	}
+	
 }

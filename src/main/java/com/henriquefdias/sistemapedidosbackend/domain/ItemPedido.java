238f0a27754b71d8bb5089091159cd1997ff3a4b
Henrique de Freitas Dias <60013916+henriquefdias@users.noreply.github.com>
2024-02-03 10:13:33 -0300

    Update ItemPedido.java

--- src/main/java/com/henriquefdias/sistemapedidosbackend/domain/ItemPedido.java
+++ src/main/java/com/henriquefdias/sistemapedidosbackend/domain/ItemPedido.java
@@ -2,6 +2,8 @@ package com.henriquefdias.sistemapedidosbackend.domain;
 
 public class ItemPedido {
 	
+	private ItemPedidoPK id = new ItemPedidoPK();
+	
 	private Double desconto;
 	private Integer quantidade;
 	private Double preco;

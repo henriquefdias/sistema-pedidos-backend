561ee262bd783e80c37f5b68adaa50de7ff0a2c4
Henrique de Freitas Dias <60013916+henriquefdias@users.noreply.github.com>
2024-02-06 08:52:30 -0300

    Update ItemPedidoPK.java

--- src/main/java/com/henriquefdias/sistemapedidosbackend/domain/ItemPedidoPK.java
+++ src/main/java/com/henriquefdias/sistemapedidosbackend/domain/ItemPedidoPK.java
@@ -4,11 +4,13 @@ import java.io.Serializable;
 import java.util.Objects;
 
 import javax.persistence.Embeddable;
+import javax.persistence.ManyToOne;
 
 @Embeddable
 public class ItemPedidoPK implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@ManyToOne
 	private Pedido pedido;
 	private Produto produto;
 	public Pedido getPedido() {
